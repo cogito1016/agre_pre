41f06113d5554142b3f854913b0aa30300624109
cogito1016 <cogito1016@naver.com>
2021-04-07 16:52:49 +0900

    feat: Feature > Carousel Slider

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,41 @@
+import React, { Component } from "react";
 import { Header, Feature, List } from "./components";
+import axios from "axios";
 
-const Home = () => {
-  return (
-    <div>
-      <Header />
-      <Feature />
-      <List />
-    </div>
-  );
-};
+const endPoint = "https://node.wingeat.com/test-api/features";
+
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      featureImgs: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getAndSetFeatureImgs();
+  }
+
+  getAndSetFeatureImgs = () => {
+    axios.get(endPoint).then((response) => {
+      const data = response.data;
+      this.setState({
+        featureImgs: data,
+      });
+    });
+  };
+
+  render() {
+    const featureImgs = this.state.featureImgs;
+
+    return (
+      <div>
+        <Header />
+        <Feature featureImgs={featureImgs} />
+        <List />
+      </div>
+    );
+  }
+}
 
 export default Home;
--- src/pages/home/components/feature/Feature.js
+++ src/pages/home/components/feature/Feature.js
@@ -1,48 +1,17 @@
 import React from "react";
-import Slider from "react-slick";
 import styled from "styled-components";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import CarouselSlider from "./items/CarouselSlider";
 
 const Container = styled.div`
-  /* position: relative;
   width: 1200px;
   margin: 0 auto;
-  background-color: gray; */
 `;
 
-const Feature = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+const Feature = (props) => {
   return (
-    <div>
-      <h2> Single Item</h2>
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
-    </div>
+    <Container>
+      <CarouselSlider featureImgs={props.featureImgs} />
+    </Container>
   );
 };
 
